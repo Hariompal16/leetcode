91da45d3afc3ac9b5c0c9d535aaf379b3e7e7f3c
Hariompal16 <ap9308719@gmail.com>
2026-07-13 09:55:39 +0530

    Time: 0 ms (100%), Space: 16.8 MB (70.31%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-int r;
-int c;
- int row[4]={-1,1,0,0};
+    int r;
+    int c;
+
+    int row[4]={-1,1,0,0};
     int col[4]={0,0,-1,1};
 
     bool valid(int i,int j){
@@ -10,41 +11,58 @@ int c;
     }
 
     int orangesRotting(vector<vector<int>>& grid) {
+
         r=grid.size();
         c=grid[0].size();
-        queue<pair<int,int>>q;
+
+        queue<pair<int,int>> q;
+
+        int fresh=0;
         int count=0;
+
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(grid[i][j]==2){
                     q.push(make_pair(i,j));
                 }
+                else if(grid[i][j]==1){
+                    fresh++;
+                }
             }
         }
+
+        if(fresh==0) return 0;
+
         while(!q.empty()){
-            count++;
+
             int size=q.size();
+            count++;
+
             for(int i=0;i<size;i++){
+
                 int new_i=q.front().first;
                 int new_j=q.front().second;
                 q.pop();
-                for(int k=0;k<4;k++){
-                    if(valid(new_i+row[k],new_j+col[k])&&grid[new_i+row[k]][new_j+col[k]]==1){
-                        grid[new_i+row[k]][new_j+col[k]]=2;
-                        q.push(make_pair(new_i+row[k],new_j+col[k]));
 
+                for(int k=0;k<4;k++){
+
+                    int ni=new_i+row[k];
+                    int nj=new_j+col[k];
+
+                    if(valid(ni,nj) && grid[ni][nj]==1){
+
+                        grid[ni][nj]=2;
+                        fresh--;
+                        q.push(make_pair(ni,nj));
                     }
                 }
             }
         }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-            }
+
+        if(fresh>0){
+            return -1;
         }
-        if(count==0) return 0;
+
         return count-1;
     }
 };
