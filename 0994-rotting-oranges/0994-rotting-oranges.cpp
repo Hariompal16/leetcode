15bf445f2eb3d834d681f3db77aab5d7d1b5acd8
Hariompal16 <ap9308719@gmail.com>
2026-08-24 22:29:17 +0530

    Time: 0 ms (100%), Space: 16.7 MB (69.58%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,68 +1,50 @@
 class Solution {
 public:
-    int r;
-    int c;
-
-    int row[4]={-1,1,0,0};
+int r;
+int c;
+ int row[4]={-1,1,0,0};
     int col[4]={0,0,-1,1};
-
-    bool valid(int i,int j){
-        return i>=0 && i<r && j>=0 && j<c;
-    }
-
+bool valid(int i,int j){
+    return i>=0&&i<r &&j>=0&&j<c;
+}
     int orangesRotting(vector<vector<int>>& grid) {
-
         r=grid.size();
         c=grid[0].size();
-
-        queue<pair<int,int>> q;
-
-        int fresh=0;
-        int count=0;
-
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+        queue<pair<int,int>>q;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==2){
                     q.push(make_pair(i,j));
                 }
-                else if(grid[i][j]==1){
-                    fresh++;
-                }
             }
         }
-
-        if(fresh==0) return 0;
-
+   int count=0;
         while(!q.empty()){
-
-            int size=q.size();
+            int s=q.size();
             count++;
+            for(int i=0;i<s;i++){
+             
+             int new_i=q.front().first;;
+             int new_j=q.front().second;
+             q.pop();
 
-            for(int i=0;i<size;i++){
-
-                int new_i=q.front().first;
-                int new_j=q.front().second;
-                q.pop();
-
-                for(int k=0;k<4;k++){
-
-                    int ni=new_i+row[k];
-                    int nj=new_j+col[k];
-
-                    if(valid(ni,nj) && grid[ni][nj]==1){
-
-                        grid[ni][nj]=2;
-                        fresh--;
-                        q.push(make_pair(ni,nj));
-                    }
+             for(int k=0;k<4;k++){
+                if(valid(new_i+row[k],new_j+col[k])&&grid[new_i+row[k]][new_j+col[k]]==1){
+                    grid[new_i+row[k]][new_j+col[k]]=2;
+                    q.push({new_i+row[k],new_j+col[k]});
                 }
+             }
+            
             }
         }
 
-        if(fresh>0){
-            return -1;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1) return -1;
+            }
         }
-
+        if(count==0) return 0;
         return count-1;
+        
     }
 };
