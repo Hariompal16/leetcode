4150990ea4e5884d4233a4ab03ed743d1149b359
Hariompal16 <ap9308719@gmail.com>
2026-08-25 10:37:02 +0530

    Time: 23 ms (83.73%), Space: 18.3 MB (39.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -8,31 +8,34 @@ bool valid(int i,int j){
     return i>=0&&i<r&&j>=0&&j<c;
 }
     int numIslands(vector<vector<char>>& grid) {
+        r=grid.size();
+        c=grid[0].size();
         queue<pair<int,int>>q;
-      r=grid.size();
-      c=grid[0].size();
-      int count=0;
-      for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            if(grid[i][j]=='1'){
-               grid[i][j]='0';
-               q.push(make_pair(i,j));
-               count++;
-               while(!q.empty()){
-                int new_i=q.front().first;
-                int new_j=q.front().second;
-                q.pop();
-                for(int k=0;k<4;k++){
-                    if(valid(new_i+row[k],new_j+col[k])&&grid[new_i+row[k]][new_j+col[k]]=='1'){
-                        grid[new_i+row[k]][new_j+col[k]]='0';
-                        q.push(make_pair(new_i+row[k],new_j+col[k]));
-                    }
+        int count=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    q.push({i,j});
+                    grid[i][j]='0';
+                    count++;
+                    while(!q.empty()){
+                    
+                    
+                        int new_i=q.front().first;
+                        int new_j=q.front().second;
+                        q.pop();
+                         for(int k=0;k<4;k++){
+                            if(valid(new_i+row[k],new_j+col[k])&&grid[new_i+row[k]][new_j+col[k]]=='1'){
+                                q.push({new_i+row[k],new_j+col[k]});
+                                grid[new_i+row[k]][new_j+col[k]]='0';
+                            }
+                         }
+                    
                 }
-               }
+                }
+                
             }
         }
-       
-      }
-       return count;  
+        return count;
     }
 };
