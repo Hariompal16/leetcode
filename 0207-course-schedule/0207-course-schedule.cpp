59daf75d4dd0ea048de13cbf5c4de84aa113ed7c
Hariompal16 <ap9308719@gmail.com>
2026-08-25 15:56:41 +0530

    Time: 8 ms (35.03%), Space: 19.1 MB (82.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,40 +2,39 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
-        for(int i=0;i<prerequisites.size();i++){
-         int course = prerequisites[i][0];
-          int pre = prerequisites[i][1];
-
-    adj[pre].push_back(course);
-}
-        queue<int>q;
-        vector<int>indegree(numCourses,0);
-        for(int i=0;i<adj.size();i++){
-            for(int j=0;j<adj[i].size();j++){
-                indegree[adj[i][j]]++;
-            }
-        }
-
-        for(int i=0;i<indegree.size();i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
         int count=0;
-        while(!q.empty()){
-            int a=q.front();
-            q.pop();
-            count++;
-            for(int i=0;i<adj[a].size();i++){
-                indegree[adj[a][i]]--;
-                if( indegree[adj[a][i]]==0){
-                    q.push(adj[a][i]);
-                }
-            }
+        queue<int>q;
+       vector<int>indegree(numCourses,0);
+        for(auto &e:prerequisites){
+            int u=e[0];
+            int v=e[1];
+
+            adj[u].push_back(v);
         }
-        if(count==numCourses){
-            return true;
+     for(int i=0;i<adj.size();i++){
+        for(int j=0;j<adj[i].size();j++){
+            indegree[adj[i][j]]++;
         }
-        return false;
+     }
+
+     for(int i=0;i<indegree.size();i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+     }
+
+     while(!q.empty()){
+        int node=q.front();
+        count++;
+        q.pop();
+    for(int i=0;i<adj[node].size();i++){
+        indegree[adj[node][i]]--;
+        if(indegree[adj[node][i]]==0){
+            q.push(adj[node][i]);
+        }
+    }
+     }
+     return count==numCourses;
+        
     }
 };
